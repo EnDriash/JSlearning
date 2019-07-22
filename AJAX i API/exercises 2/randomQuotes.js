77d1e63c64a171517101ce95f159b6525080661c
Jędrzej Krawczyk <siewierjedr@gmail.com>
2019-07-22 10:35:38 +0200

    Added little correction

--- AJAX i API/exercises 2/randomQuotes.js
+++ AJAX i API/exercises 2/randomQuotes.js
@@ -1,4 +1,6 @@
+'use strict'
 (function(){
+
 var tweetLink = "https://twitter.com/intent/tweet?text=";
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 var prefix = "https://cors-anywhere.herokuapp.com/";
