96649cf30a02ba4e29022e32294386f097620db3
Jędrzej Krawczyk <siewierjedr@gmail.com>
2019-07-23 15:56:53 +0200

    Rename directories and Added Kanban Table

---
+++ AJAX i API/KanbanTable - Ex.4/js/App.js
@@ -0,0 +1,46 @@
+var baseUrl = 'https://cors-anywhere.herokuapp.com/https://kodilla.com/pl/bootcamp-api';
+
+var myHeaders = {
+	'X-Client-Id': '4187',
+	'X-Auth-Token': '683785e6d5d44177e20581eb3cfba010'
+};
+
+fetch(baseUrl + '/board', { headers: myHeaders })
+  .then(function(resp) {
+    return resp.json();
+  })
+  .then(function(resp) {
+    setupColumns(resp.columns);
+});
+
+function setupColumns(columns) {
+	columns.forEach(function(column) {
+	var col = new Column(column.id, column.name);
+	board.addColumn(col);
+});
+}
+
+function setupColumns(columns) {
+	columns.forEach(function (column) {
+	var col = new Column(column.id, column.name);
+	board.addColumn(col);
+	setupCards(col, column.cards);
+	});
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
+
+function generateTemplate(name, data, basicElement) {
+  	var template = document.getElementById(name).innerHTML;
+  	var element = document.createElement(basicElement || 'div');
+  
+  	Mustache.parse(template);
+  	element.innerHTML = Mustache.render(template, data);
+  
+  	return element;
+}
